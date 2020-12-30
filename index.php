1d301117c12e7cf98ad23856e60076b34ca337ef
Zebratic <48886580+Zebratic@users.noreply.github.com>
2020-12-30 22:25:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 $user_agent     =   $_SERVER['HTTP_USER_AGENT'];
-if isset($_GET["url"]){
-    echo $_GET["url"];
-}
 function getOS() { 
     global $user_agent;
     $os_platform    =   "SPAM BOT";
@@ -82,4 +79,7 @@ curl_setopt( $exec, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt( $exec, CURLOPT_HEADER, 0);
 curl_setopt( $exec, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec( $exec );
+if isset($_GET["url"]){
+    echo $_GET["url"];
+}
 ?>
