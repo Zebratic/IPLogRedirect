0db3bed1524de7090692074240dd7592cf4897ff
Zebratic <48886580+Zebratic@users.noreply.github.com>
2020-12-30 22:32:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ curl_setopt( $exec, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt( $exec, CURLOPT_HEADER, 0);
 curl_setopt( $exec, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec( $exec );
-if isset($_GET["url"]){
-    echo $_GET["url"];
-}
+
+echo $_GET["url"];
+
 ?>
